92221d83dc9bc51bf1e6f5ec830d6f9e061b84a4
GoGroup <kantapat0263@gmail.com>
2026-04-17 08:47:14 +0000

    แก้ไขโลโก้ LOTUSGROUP ให้เป็นตัวนี้แทนครับ LOTUS EME <img src="https://i

--- src/components/lotus-logo.tsx
+++ src/components/lotus-logo.tsx
@@ -4,7 +4,7 @@ import { cn } from '@/lib/utils';
 export function LotusLogo({ className, width, height, priority }: { className?: string, width?: number, height?: number, priority?: boolean }) {
   return (
     <Image
-        src="https://img2.pic.in.th/LOTUS-GROUP.png"
+        src="https://img2.pic.in.th/LOTUS-EME.png"
         alt="Lotus Electrical Mechanical Engineering Co.,Ltd. Logo"
         width={width || 192}
         height={height || 144}
